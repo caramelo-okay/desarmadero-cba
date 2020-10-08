bcf895227368f186dff0aa69039eb8c7a2b32a6e
Leo.Rmn <leonardo.rmn@gmail.com>
2020-10-08 10:21:17 -0300

    Update Codeigniter last version

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -5,11 +5,8 @@ use App\Models\UserModel;
 
 class Login extends BaseController
 {
-
-
     public function __construct(){
 		helper ('form');
-		
 	}
 
 
@@ -26,19 +23,23 @@ class Login extends BaseController
         /*
          * Comprueba que el usuario existe y ademas este habilitado (estado > 0)
          */
-        function validate_usr(){
-            
+        function validate_usr()
+        {
             $userModel = new UserModel();
             $request = \Config\Services::request();
+
+            $data['title'] = "Vista de usuario";
+         
             /* Aca recuperamos del form el mail como usr y el nombre como pass */
             $usrname = $request->getPost('username');
             $usrpass = $request->getPost('password');
             
-            $usuario = $userModel->where('email',$usrname)->find();
-            $title = "Vista de usuario";
-            $data = array('users'=>$usuario,'title'=>$title);
+            $data['usuario'] = $userModel->where('email',$usrname)->find();
+            
+            //$data = array('users'=>$usuario,'title'=>$title);
             
             $estructura = view('template/header',$data).view('template/v_cont',$data);
+            
             return $estructura;
         }
 }
